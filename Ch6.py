35cd77b53bdda423ae70eaae5961cd8df22737c5
Evan Oman <evan058@gmail.com>
2017-03-08 17:49:22 -0600

    Working on Ch7

--- Ch6.py
+++ Ch6.py
@@ -14,27 +14,28 @@ from collections import Counter
 def random_kid():
     return random.choice(["boy", "girl"])
     
-both_girls = 0
-older_girl = 0
-either_girl = 0
-
-random.seed(0)
-
-for _ in range(10000):
-    younger = random_kid()
-    older = random_kid()
+if __name__ == "__main__":
+    both_girls = 0
+    older_girl = 0
+    either_girl = 0
     
-    if older == "girl":
-        older_girl += 1
+    random.seed(0)
     
-    if older == "girl" and younger == "girl":
-        both_girls += 1
+    for _ in range(10000):
+        younger = random_kid()
+        older = random_kid()
         
-    if older == "girl" or younger == "girl":
-        either_girl += 1
+        if older == "girl":
+            older_girl += 1
         
-print "P(both | older) = %f" % (both_girls / older_girl) # .514 ~ 1/2
-print "P(both | either) = %f" % (both_girls / either_girl) # .341 ~ 1/3
+        if older == "girl" and younger == "girl":
+            both_girls += 1
+            
+        if older == "girl" or younger == "girl":
+            either_girl += 1
+            
+    print "P(both | older) = %f" % (both_girls / older_girl) # .514 ~ 1/2
+    print "P(both | either) = %f" % (both_girls / either_girl) # .341 ~ 1/3
 
 ### Normal Distribution ###
 
@@ -42,15 +43,16 @@ def normal_pdf(x, mu = 0, sigma = 1):
     sqrt_two_pi = math.sqrt(2*math.pi)
     return (math.exp(-(x-mu)**2/(2*sigma**2))/(sqrt_two_pi*sigma))
     
-xs = [x / 10.0 for x in range(-50,50)]
-plt.figure(1)
-plt.plot(xs, [normal_pdf(x, sigma=1) for x in xs], '-', label='mu=0,sigma=1')
-plt.plot(xs, [normal_pdf(x, sigma=2) for x in xs], '4--', label='mu=0,sigma=2')
-plt.plot(xs, [normal_pdf(x, sigma=.5) for x in xs], ':', label='mu=0, sigma=.5')
-plt.plot(xs, [normal_pdf(x, mu=-1) for x in xs], '-.', label='mu=-1,sigma=1')
-plt.legend()
-plt.title("Various normal pdfs")
-plt.show()
+if __name__ == "__main__":
+    xs = [x / 10.0 for x in range(-50,50)]
+    plt.figure(1)
+    plt.plot(xs, [normal_pdf(x, sigma=1) for x in xs], '-', label='mu=0,sigma=1')
+    plt.plot(xs, [normal_pdf(x, sigma=2) for x in xs], '4--', label='mu=0,sigma=2')
+    plt.plot(xs, [normal_pdf(x, sigma=.5) for x in xs], ':', label='mu=0, sigma=.5')
+    plt.plot(xs, [normal_pdf(x, mu=-1) for x in xs], '-.', label='mu=-1,sigma=1')
+    plt.legend()
+    plt.title("Various normal pdfs")
+    plt.show()
 
 # The normal CDF is not normally expressable in an "elementary" manner, we can 
 # use math.erf to compute though
@@ -58,16 +60,17 @@ plt.show()
 def normal_cdf(x, mu=0, sigma=1):
     return (1 + math.erf((x-mu)/(math.sqrt(2) * sigma)))/2
     
-xs = [x / 10.0 for x in range(-50,50)]
-plt.figure(2)
-plt.plot(xs, [normal_cdf(x, sigma=1) for x in xs], '-', label='mu=0,sigma=1')
-plt.plot(xs, [normal_cdf(x, sigma=2) for x in xs], '--', label='mu=0,sigma=2')
-plt.plot(xs, [normal_cdf(x, sigma=.5) for x in xs], ':', label='mu=0, sigma=.5')
-plt.plot(xs, [normal_cdf(x, mu=-1) for x in xs], '-.', label='mu=-1,sigma=1')
-plt.legend()
-plt.title("Various normal cdfs")
-plt.show()
-
+if __name__ == "__main__":
+    xs = [x / 10.0 for x in range(-50,50)]
+    plt.figure(2)
+    plt.plot(xs, [normal_cdf(x, sigma=1) for x in xs], '-', label='mu=0,sigma=1')
+    plt.plot(xs, [normal_cdf(x, sigma=2) for x in xs], '--', label='mu=0,sigma=2')
+    plt.plot(xs, [normal_cdf(x, sigma=.5) for x in xs], ':', label='mu=0, sigma=.5')
+    plt.plot(xs, [normal_cdf(x, mu=-1) for x in xs], '-.', label='mu=-1,sigma=1')
+    plt.legend()
+    plt.title("Various normal cdfs")
+    plt.show()
+    
 # Inverting the cdf algebraically is not nice, but since it is monotonically 
 # increasing, we can use binary search 
 def inverse_normal_cdf(p, mu=0, sigma=1, tolerance = .00001):
@@ -129,4 +132,5 @@ def make_hist(p, n, num_points):
     plt.title("Binomial Distribution vs. Normal Approximation")
     plt.show()
     
-make_hist(.75, 100, 10000)
+if __name__ == "__main__":
+    make_hist(.75, 100, 10000)
---
+++ Ch7.py
@@ -0,0 +1,80 @@
+# Chapter 7 Hypothesis and Inference
+
+from __future__ import division
+import Ch6
+import math
+
+# We want to check whether a given coin is fair. So we make our null hypothesis
+# that the coin is fair (p = .5) and the alternative hypothesis that the coin is
+# not fair (p =/= .5)
+
+# Our test will then involve flipping the coin n times. We use the bernoulli 
+# trials and normal approximation explored in Chapter 6 
+
+def normal_approximation_to_binomial(n, p):
+    """Finds mu and sigma corresponding to a Binomial(n, p)"""
+    mu = p * n
+    
+    # p*q*n
+    sigma = math.sqrt(p*(1-p)*n)
+    
+    return mu, sigma
+    
+# probability that variable is below threshold
+normal_probability_below = Ch6.normal_cdf
+
+# it is above the threshold if it isn't below the threshold
+def normal_probabilty_above(lo, mu=0, sigma=1):
+    return 1 - Ch6.normal_cdf(lo, mu, sigma)
+    
+# it is in between if it is less than hi but not let than lo
+def normal_probability_between(lo, hi, mu=0, sigma=1):
+    return Ch6.normal_cdf(hi, mu, sigma) - Ch6.normal_cdf(lo, mu, sigma)
+    
+# it is outside if it is not in between
+def normal_probability_outside(lo, hi, mu=0, sigma=1):
+    return 1 - normal_probability_between(lo, hi, sigma, mu)
+    
+
+# We can also perform the reverse of all the above operations to find z values
+# corresponding to certain p ranges
+
+def normal_upper_bound(probability, mu=0, sigma=1):
+    """returns the z for which p(Z <= z) = probability"""
+    return Ch6.inverse_normal_cdf(probability, mu, sigma)
+    
+def normal_lower_bound(probability, mu=0, sigma=1):
+    """returns the z for which p(Z >= z) = probability"""
+    return Ch6.inverse_normal_cdf(1 - probability, mu, sigma)
+    
+def normal_two_sided_bounds(probability, mu=0, sigma=1):
+    """returns the symmetric(about the mean) bounds that contain specified p"""
+    tail_probability = (1 - probability) / 2
+    
+    # upper bound should have tail_probability above it
+    upper_bound = normal_lower_bound(tail_probability, mu, sigma)
+    
+    
+    # lower bound should have tail_probability below it
+    lower_bound = normal_upper_bound(tail_probability, mu, sigma)
+    
+    return lower_bound, upper_bound
+    
+    
+if __name__ == "__main__":
+    # In particular, suppose we were to flip a coin 100 times. If our hypothesis is 
+    # true, X should be distributed approximately normally iwth mean 50 and std 15.8
+    mu_0, sigma_0 = normal_approximation_to_binomial(1000,.5)
+    
+    # We now need to decide on a level of significance, that is how willing are
+    # we to make a type I, false positive error with respect to H_0
+    
+    # Suppose H_0 falls out the following bounds:
+    normal_two_sided_bounds(.95, sigma_0, mu_0) # (469, 531)
+    
+    # Then we say that we reject the null hypothesis because we should be 95% 
+    # confident that the mean should lie within the range (469, 531)
+    
+    
+    
+    
